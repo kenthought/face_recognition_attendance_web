85a81ddcb90b32d9748217517fea7d50d5c90c8d
kenthought <nananaupan@gmail.com>
2022-05-23 00:26:33 +0800

    setting up the component

--- resources/js/components/AttendanceComponent.vue
+++ resources/js/components/AttendanceComponent.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Attendance</div>
-                    <div class="card-body">
-                        Attendance
-                    </div>
-                </div>
-            </div>
+    <div class="pr-3">
+        <h2 class="text-normal mt-3 mb-3">Attendance</h2>
+        <div class="row justify-content-center mt-4 p-3">
+            <b-table
+                show-empty
+                striped
+                hover
+                responsive
+                :fields="fields"
+                :items="items"
+                label-sort-asc=""
+                label-sort-desc=""
+                label-sort-clear=""
+                @row-clicked="onRowClicked"
+            ></b-table>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    },
+};
 </script>
